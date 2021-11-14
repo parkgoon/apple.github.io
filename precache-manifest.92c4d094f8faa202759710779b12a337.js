a07ae0be99e18706d83bdd6d2b3027c8e8dcf312
ohdury <ohdury@gmail.com>
2021-11-14 15:30:50 +0900

    init nba beta

---
+++ precache-manifest.92c4d094f8faa202759710779b12a337.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b98ae1d16f420d0fc126844637ccd014",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72483964ef80633e080d",
+    "url": "/static/css/main.bc821aff.chunk.css"
+  },
+  {
+    "revision": "a6c6d29809cc055b3af7",
+    "url": "/static/js/2.2fb6810f.chunk.js"
+  },
+  {
+    "revision": "e1f5ad522e1b15731a197887d22f1a58",
+    "url": "/static/js/2.2fb6810f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72483964ef80633e080d",
+    "url": "/static/js/main.1f063225.chunk.js"
+  },
+  {
+    "revision": "2a3496e0349049e73fa1",
+    "url": "/static/js/runtime-main.9aef5679.js"
+  }
+]);
